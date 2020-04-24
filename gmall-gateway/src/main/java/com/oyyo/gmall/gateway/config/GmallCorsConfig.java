e43a8ae046747069a386453fc833745ed75a159b
leeq <48358720+leikui@users.noreply.github.com>
2020-04-24 18:55:37 +0800

    Solve cross domain problems

---
+++ gmall-gateway/src/main/java/com/oyyo/gmall/gateway/config/GmallCorsConfig.java
@@ -0,0 +1,41 @@
+package com.oyyo.gmall.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @ClassName: GmallCorsConfig
+ * @Description:  cors 跨域配置
+ * @Author: LiKui
+ * @Date: 2020-4-24 15:57
+ * @Version: 1.0
+ */
+@Configuration
+public class GmallCorsConfig {
+
+    //允许跨域的域名
+    @Value("${origin}")
+    private static  String origin ;
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        //跨域配置对象
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:1000");
+        //是否允许携带cookie
+        corsConfiguration.setAllowCredentials(true);
+        //允许所有请求方法
+        corsConfiguration.addAllowedMethod("*");
+        //允许所有头信息
+        corsConfiguration.addAllowedHeader("*");
+
+        //配置源对象
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**",corsConfiguration);
+        //返回一个cors 过滤器对象
+        return new CorsWebFilter(configurationSource);
+    }
+}
