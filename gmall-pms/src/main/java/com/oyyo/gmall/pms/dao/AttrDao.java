b7f88882406678464564be1a095376f0edbc3f57
oy <oyyo@163.com>
2020-05-05 23:36:53 +0800

    delete attrdao

---
+++ gmall-pms/src/main/java/com/oyyo/gmall/pms/dao/AttrDao.java
@@ -0,0 +1,17 @@
+package com.oyyo.gmall.pms.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.oyyo.gmall.pms.entity.AttrEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品属性
+ * 
+ * @author oy
+ * @email oy@lcd.com
+ * @date 2020-05-05 22:41:39
+ */
+@Mapper
+public interface AttrDao extends BaseMapper<AttrEntity> {
+	
+}
